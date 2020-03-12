8aba812f5d78e71eefa4afe409cbe664c8b02d10
procoder0001 <weichen.1994.0613@gmail.com>
2020-03-12 22:31:19 +0000

    Setup main html, body styles

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
-import './index.css';
+import './index.scss';
 import App from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));

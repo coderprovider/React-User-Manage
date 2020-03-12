a51241456777b3318875c0fe9a566f83b9982d5b
procoder0001 <weichen.1994.0613@gmail.com>
2020-03-12 22:33:03 +0000

    Improve App styling with flexbox

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import './App.scss';
 import './Header/Header'
 import Header from "./Header/Header";
+import MainView from "./MainView/MainView";
 
 function App() {
     return (
         <div className="App">
             <Header/>
+            <MainView/>
         </div>
     );
 }
